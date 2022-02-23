f9283a5192e8a3d0193bfc3c74f259e2604d9a3b
Kuma-inataku <inataku0121@gmail,com>
2022-02-23 13:44:31 +0900

    first commit

---
+++ src/components/ImageChildComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <!-- OK時の画像 -->
+    <div
+      v-if="url"
+    >
+      <h2>OK!!!</h2>
+      <img
+        :src="url"
+        width=200
+      >
+    </div>
+    <!-- NG時の画像 -->
+    <div
+      v-else
+    >
+      <h2>NG...</h2>
+      <img
+        src="../assets/no-image.png"
+        width=200
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+  // data() {
+  //   return {
+  //     childUrl: '',
+  //   }
+  // },
+  beforeCreate() {
+    console.log('beforeCreate: ', this.url);
+    // const newUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==';
+    // this.$emit("replace", newUrl);
+  },
+  created() {
+    console.log('created: ', this.url);
+    // const newUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==';
+    // this.$emit("replace", newUrl);
+  },
+  beforeMount() {
+    console.log('beforeMount: ', this.url);
+    // const newUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==';
+    // this.$emit("replace", newUrl);
+  },
+  mounted() {
+    console.log('mounted: ', this.url);
+    // const newUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==';
+    // this.$emit("replace", newUrl);
+  },
+  methods: {
+    getUrl() {
+      console.log('methods(getUrl): ', this.url);
+      // const newUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==';
+      // this.$emit("replace", newUrl);
+      return this.url;
+    }
+  }
+}
+</script>
---
+++ src/components/ImageParentComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ImageChildComponent
+      :url="url"
+      @replace="replaceUrl"
+    ></ImageChildComponent>
+  </div>
+</template>
+
+<script>
+import ImageChildComponent from './ImageChildComponent.vue'
+
+export default {
+  components: {
+    ImageChildComponent
+  },
+  data() {
+    return {
+      url: '',
+    }
+  },
+  methods: {
+    replaceUrl(newUrl) {
+      console.log(newUrl);
+      this.url = newUrl;
+    },
+  }
+}
+</script>
