c3854302f5cfa80abedff93572a5d2b6d59daf89
Kuma-inataku <inataku0121@gmail,com>
2022-04-17 17:01:40 +0900

    comment追加

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -13,10 +13,13 @@
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs/legacy'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+// Note: 公式Docには"1.xでは/legacyが必要"とあるが、Githubにある"For Vue 2 projects, you need to import from vue-chartjs/legacy."が正
+// @see(Github): https://github.com/apertureless/vue-chartjs#:~:text=For%20Vue%202%20projects%2C%20you%20need%20to%20import%20from%20vue%2Dchartjs/legacy
+// @see(公式Doc): https://vue-chartjs.org/ja/guide/#:~:text=js%402.9.4%20%2D%2Dsave-,TIP,-Vue.js%E3%81%AE
+import { Bar } from 'vue-chartjs/legacy';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
   name: 'BarChart',
